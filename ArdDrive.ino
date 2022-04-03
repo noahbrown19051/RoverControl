2be755e7a3b16c4bcb8ad41f9960547ff93e19c0
Noah Brown <67166579+noahbrown19051@users.noreply.github.com>
2022-04-03 15:58:29 -0600

    Add files via upload

--- ArdDrive.ino
+++ ArdDrive.ino
@@ -44,19 +44,19 @@ void backward()
 }
 void left()
 {
-  motor1.run(FORWARD);
+  motor1.run(BACKWARD);
   motor2.run(RELEASE);
   motor3.run(RELEASE);
-  motor4.run(FORWARD);
+  motor4.run(BACKWARD);
   motor1.setSpeed(speedCar);
   motor4.setSpeed(speedCar);
 }
 void right()
 {
-  motor1.run(RELEASE);
+  motor1.run(BACKWARD);
   motor2.run(FORWARD);
   motor3.run(FORWARD);
-  motor4.run(RELEASE);
+  motor4.run(BACKWARD);
   motor2.setSpeed(speedCar);
   motor3.setSpeed(speedCar);
 }
